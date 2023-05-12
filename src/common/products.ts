925e64f1611131950d2467146f1bb635a35ce12b
Thanhph28098 <thanhthaolam1508@gmail.com>
2023-05-12 22:06:46 +0700

    first commit

---
+++ src/common/products.ts
@@ -0,0 +1,7 @@
+export interface IProducts {
+  id: number;
+  name: string;
+  price: number;
+  desc: string;
+  imageUrl: string;
+}
---
+++ src/common/user.ts
@@ -0,0 +1,6 @@
+export interface IUser {
+  name: string;
+  age: 20;
+  msv: string;
+  gender: string;
+}
