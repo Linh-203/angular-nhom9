537f3ee41ebd80b5aa88e4ef6fc755ac9083adfc
Linh-203 <hlinh2003.5.9@gmail.com>
2023-05-13 15:05:18 +0700

    abc

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-<<<<<<< HEAD
-
-const routes: Routes = [];
-=======
 import { HomeComponent } from './pages/home/home.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
   // {path: '', component: }
 ];
->>>>>>> 960a139af327697394938be735c21ca168ff8933
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,8 @@ import { HomeComponent } from './pages/home/home.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from './components/header/header.component';
 import { AdminComponent } from './components/admin/admin.component';
-<<<<<<< HEAD
-=======
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
->>>>>>> 960a139af327697394938be735c21ca168ff8933
 
 @NgModule({
   declarations: [
@@ -20,11 +17,8 @@ import { RegisterComponent } from './components/register/register.component';
     FooterComponent,
     HeaderComponent,
     AdminComponent,
-<<<<<<< HEAD
-=======
     LoginComponent,
     RegisterComponent,
->>>>>>> 960a139af327697394938be735c21ca168ff8933
   ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
