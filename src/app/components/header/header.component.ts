7731a51b38afdee40465b438670d9197c04f3af1
screaminGreen <101007489+hiepfre3101@users.noreply.github.com>
2023-05-28 17:02:04 +0700

    register done & confirm password has bug

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,32 +1,33 @@
-import { Component } from '@angular/core';
-import { ProductInCartComponent } from '../product-in-cart/product-in-cart.component';
-import { productsFake } from 'src/data/products';
-import {favoriteProductsFake } from 'src/data/products';
-import { category } from 'src/data/products';
+import { Component } from '@angular/core'
+import { ProductInCartComponent } from '../product-in-cart/product-in-cart.component'
+import { productsFake } from 'src/data/products'
+import { favoriteProductsFake } from 'src/data/products'
+import { category } from 'src/data/products'
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css'],
+   selector: 'app-header',
+   templateUrl: './header.component.html',
+   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
-  slides = [
-    {
-      image: 'https://via.placeholder.com/600x400',
-      title: 'Slide 1',
-      text: 'This is slide 1',
-    },
-    {
-      image: 'https://via.placeholder.com/600x400',
-      title: 'Slide 2',
-      text: 'This is slide 2',
-    },
-    {
-      image: 'https://via.placeholder.com/600x400',
-      title: 'Slide 3',
-      text: 'This is slide 3',
-    },
-  ];
-
-  category = category
-  product = favoriteProductsFake
+   slides = [
+      {
+         image: 'https://via.placeholder.com/600x400',
+         title: 'Slide 1',
+         text: 'This is slide 1'
+      },
+      {
+         image: 'https://via.placeholder.com/600x400',
+         title: 'Slide 2',
+         text: 'This is slide 2'
+      },
+      {
+         image: 'https://via.placeholder.com/600x400',
+         title: 'Slide 3',
+         text: 'This is slide 3'
+      }
+   ]
+   token = localStorage.getItem('token')
+   userInfo = JSON.parse(localStorage.getItem('user')!)
+   category = category
+   product = favoriteProductsFake
 }
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,45 +1,34 @@
-import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
+import { AuthService } from './../../pages/login-register/auth.service';
+import { Component, OnInit } from '@angular/core'
+import { FormBuilder, FormGroup, Validators } from '@angular/forms'
+import { ILogin, ISignup } from 'src/common/user'
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css'],
+   selector: 'app-login',
+   templateUrl: './login.component.html',
+   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-  public loginForm!: FormGroup;
-  constructor(
-    private FormBuilder: FormBuilder,
-    private http: HttpClient,
-    private router: Router
-  ) {}
-  ngOnInit(): void {
-    this.loginForm = this.FormBuilder.group({
-      email: ['', Validators.required],
-      password: ['', Validators.required],
-    });
-  }
-  login() {
-    this.http.get<any>('http://localhost:3000/signUpUsers').subscribe(
-      (res) => {
-        const user = res.find((a: any) => {
-          return (
-            a.email === this.loginForm.value.email &&
-            a.password === this.loginForm.value.password
-          );
-        });
-        if (user) {
-          alert('login successfully');
-          this.loginForm.reset();
-          this.router.navigate(['']);
-        } else {
-          alert('login failed, user not found');
-        }
-      },
-      (err) => {
-        alert('something went wrong');
+   public loginForm!: FormGroup
+   constructor(private FormBuilder: FormBuilder, private loginService: AuthService) {}
+   ngOnInit(): void {
+      this.loginForm = this.FormBuilder.group({
+         email: ['', Validators.required],
+         password: ['', Validators.required]
+      })
+   }
+   dataSubmit: ILogin = {
+      email: '',
+      password: ''
+   }
+   handleChange(event: any) {
+      this.dataSubmit = { ...this.dataSubmit, [event.target.name]: event.target.value }
+   }
+   signIn(data: ILogin) {
+      try {
+         console.log(data)
+         return this.loginService.signIn(data)
+      } catch (error) {
+         console.log(error)
       }
-    );
-  }
+   }
 }
