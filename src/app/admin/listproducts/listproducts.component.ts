d344e10c4196b78234a3fb00ecc7b6cac5a1e843
Linh-203 <hlinh2003.5.9@gmail.com>
2023-06-07 16:06:07 +0700

    update category: thêm danh mục mặc định(chứa các sp thuộc các danh mục bị xóa) + sửa linh tinh

--- src/app/admin/listproducts/listproducts.component.ts
+++ src/app/admin/listproducts/listproducts.component.ts
@@ -30,26 +30,20 @@ export class ListproductsComponent {
       })
    }
 
-   //   deleteProduct(product: any) {
-   //   const index = this.adminProducts.indexOf(product);
-   //   if (index >=0 ) {
-   //     this.adminProducts.splice(index, 1);
-   //   }
-   // }
-
    deleteProduct(id: any): void {
-      const apiUrl = `http://localhost:8000/api/products/${id}`
-      this.http
-         .delete(apiUrl, {
-            headers: {
-               authorization: 'Bearer' + JSON.stringify(localStorage.getItem('token'))
-            }
-         })
-         .subscribe((res: any) => {
-            console.log(res)
-
-            // xóa sản phẩm khỏi danh sách hiển thị
-            this.adminProducts = this.adminProducts.filter((product: any) => id !== product._id)
-         })
+      const confirm = window.confirm('bạn chắc chưa')
+      if (confirm) {
+         const apiUrl = `http://localhost:8000/api/products/${id}`
+         this.http
+            .delete(apiUrl, {
+               headers: {
+                  authorization: 'Bearer' + JSON.stringify(localStorage.getItem('token'))
+               }
+            })
+            .subscribe((res: any) => {
+               console.log(res)
+               this.adminProducts = this.adminProducts.filter((product: any) => id !== product._id)
+            })
+      }
    }
 }
