c5f353eac07695f9cd65a8aa48e98d870041cafe
screaminGreen <101007489+hiepfre3101@users.noreply.github.com>
2023-06-08 22:59:10 +0700

    search in header done

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -46,6 +46,8 @@ export class HeaderComponent implements OnInit {
    handleOpenSearch() {
       this.dialog.open(SearchComponent, { disableClose: true })
    }
+   
+
    slides = [
       {
          image: 'https://via.placeholder.com/600x400',
